1a62e84d2ae042444a1c33360f4249a76fd381f3
Abdeljalil ZOGHLAMI <abdeldjalil.zoghlami@ccssd.cnrs.fr>
2025-12-30 16:56:22 +0100

    add user

---
+++ service-app/src/Common/AppEnum/UserRolesEnum.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace App\Common\AppEnum;
+
+enum UserRolesEnum: string
+{
+    case ROLE_ADMIN = 'ROLE_ADMIN';
+    case ROLE_USER = 'ROLE_USER';
+}
--- service-app/src/Example/Actions/ExampleAction.php
+++ service-app/src/Example/Actions/ExampleAction.php
@@ -14,6 +14,7 @@ final class ExampleAction
     public function __invoke(Request $request): JsonResponse
     {
         $data = $request->request->all();
+
         return new JsonResponse(['message' => 'Action executed', 'data' => $data]);
     }
 }
