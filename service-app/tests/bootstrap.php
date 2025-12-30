e92779b586e3739e10072223bf42d61507f40da7
Abdeljalil ZOGHLAMI <abdeldjalil.zoghlami@ccssd.cnrs.fr>
2025-12-30 10:48:54 +0100

    tmp

--- service-app/tests/bootstrap.php
+++ service-app/tests/bootstrap.php
@@ -7,7 +7,8 @@ require dirname(__DIR__).'/vendor/autoload.php';
 if (method_exists(Dotenv::class, 'bootEnv')) {
     (new Dotenv())->bootEnv(dirname(__DIR__).'/.env');
 }
-/** @phpstan-ignore if.condNotBoolean */
+
+/* @phpstan-ignore if.condNotBoolean */
 if ($_SERVER['APP_DEBUG']) {
     umask(0000);
 }
