7f9cf3f3b72f9dc8798ae6918f0cfcfc745758fc
Abdeljalil ZOGHLAMI <abdeldjalil.zoghlami@ccssd.cnrs.fr>
2025-12-31 09:39:05 +0100

    qa

--- service-app/src/Controller/HomeController.php
+++ service-app/src/Controller/HomeController.php
@@ -2,15 +2,9 @@
 
 namespace App\Controller;
 
-use App\Entity\User;
-use App\Form\LoginType;
-use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
-use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
-use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
 use Symfony\Component\Routing\Attribute\Route;
-use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
 
 class HomeController extends AbstractController
 {
